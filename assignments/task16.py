a00c2cec6c5f16268d53a9fa635dc764e31e18c0
Daksh Gandhi <dakshgandhi2007@gmail.com>
2026-09-16 10:38:31 +0530

    Delete assignments/task16.py

--- assignments/task16.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-cgpa = float(input("Enter your CGPA: "))
-print(f"My name is {name}. I am {age} years old and my CGPA is {cgpa}.")
